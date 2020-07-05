0328770d3027ee90a5c401504f6ab1cbda76046b
EdwinArevalo <edwin.arevalo@tecsup.edu.pe>
2020-07-04 21:00:09 -0500

    docs added

---
+++ docs/precache-manifest.8c54090b61d91bb05fcd6833b4886d3d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c86dd52f3645624c8ece91d482a77fc2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "379346246e2f0f2f1416",
+    "url": "/static/css/main.2330421f.chunk.css"
+  },
+  {
+    "revision": "85a4a86f3fe06f17fcc7",
+    "url": "/static/js/2.8249eaeb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8249eaeb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "379346246e2f0f2f1416",
+    "url": "/static/js/main.eb2a82a6.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
